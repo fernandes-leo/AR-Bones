141f4808ae86d64c8432748105def4ed7698439b
Leonardo Fernandes <fernandes.leo@live.com>
2022-06-24 09:00:46 -0300

    Initial Commit

---
+++ Shared/ProjetoTesteApp.swift
@@ -0,0 +1,17 @@
+//
+//  ProjetoTesteApp.swift
+//  Shared
+//
+//  Created by Cubix on 23/06/22.
+//
+
+import SwiftUI
+
+@main
+struct ProjetoTesteApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Shared/SkeletonJoint.swift
@@ -0,0 +1,14 @@
+//
+//  SkeletonJoint.swift
+//  ProjetoTeste (iOS)
+//
+//  Created by Cubix on 23/06/22.
+//
+
+import Foundation
+
+struct SkeletonJoint {
+    let name: String
+    var position: SIMD3<Float>
+    
+}
